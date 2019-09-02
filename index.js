4fe13bcbf51324bdd744e698fbfd7e4d13fbb9a4
Emma Scott <ejms236@gmail.com>
2019-09-02 15:55:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,3 @@ function logShout(string) {
   var togla = string.toUpperCase()
   console.log(togla)
 }
-
-// function DEFINITION
-function emma(num) {
-  return num + 10
-}
-
-// FUNCTION CALL
-var mynumber = 5
-emma(mynumber)
